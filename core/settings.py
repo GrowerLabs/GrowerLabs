543749e2d6786d73f035f225a39bf29aa9f2f87b
algogeek <apara20_ug@ece.nits.ac.in>
2021-10-07 19:08:04 +0530

    email

--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'growerlabs@gmail.com'
+EMAIL_HOST_PASSWORD = 'xogooaqexzlctxai'
+EMAIL_USE_TLS = True
--- growerlabs/views.py
+++ growerlabs/views.py
@@ -1,8 +1,39 @@
 from django.shortcuts import render
 
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.template.loader import render_to_string
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name", "")       
+        email = request.POST.get("email", "")
+        phone_no = request.POST.get("phone_no", "")
+        message = request.POST.get("message", "")
+
+        email_context = {
+            "name":name,
+            "email":email,
+            "phone_no":phone_no,
+            "message":message
+            
+        }
+        template_email = render_to_string('email_template.html', email_context)
+
+        email_obj = EmailMessage(
+
+            name + "has sent an email",
+            template_email,
+            settings.EMAIL_HOST_USER,
+            ['aparabiswas28@gmail.com', 'kalitaluna28@gmail.com']
+
+        )
+        email_obj.fail_silently = False
+        email_obj.send()
+        # print(email_obj)
+
     return render(request, 'contact.html')
